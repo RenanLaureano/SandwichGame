1c8ec427f1ab52aa715d3337fee6276fb1222c88
Renan Laureano <renanlaureano@gmail.com>
2021-10-15 22:11:54 -0300

    change: Add design patterns Singleton and ServiceLocator

---
+++ Assets/Game/Helper/ServiceLocator.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System;
+
+public class ServiceLocator : Singleton<ServiceLocator>
+{
+    private Dictionary<Type, System.Object> mObjects;
+
+    protected ServiceLocator()
+    {
+        mObjects = new Dictionary<Type, object>();
+    }
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+    public bool IsRegistered<T>()
+    {
+        return mObjects.ContainsKey(typeof(T));
+    }
+    public void Register<T>(T thing)
+    {
+        if (thing != null)
+        {
+            mObjects[typeof(T)] = thing;
+        }
+    }
+    public T GetComponentRegistered<T>() where T : Component
+    {
+        var type = typeof(T);
+        if (!mObjects.ContainsKey(type))
+        {
+            return null;
+        }
+
+        return (T)mObjects[typeof(T)];
+    }
+}
